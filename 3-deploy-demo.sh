b8f739d52c981adbe6865144e4ee3204e8df6963
Erkan Erol <erkanerol92@gmail.com>
2021-07-28 12:33:17 +0300

    Update 3-deploy-demo.sh

--- 3-deploy-demo.sh
+++ 3-deploy-demo.sh
@@ -6,10 +6,10 @@ oc -n openshift-cnv delete pod downloads --ignore-not-found
 oc -n openshift-cnv delete service downloads --ignore-not-found
 oc -n openshift-cnv delete route downloads --ignore-not-found
 
-oc -n openshift-cnv run downloads --image="${IMAGE}"  \
-    --restart=OnFailure --port 80 --expose
+oc -n openshift-cnv run downloads --image="quay.io/erkanerol/virt-artifacts-server:v1"  \
+    --restart=OnFailure --port 8080 --expose
 
 
-oc -n openshift-cnv create route edge --service=downloads --port=80
+oc -n openshift-cnv create route edge --service=downloads --port=8080
 
 oc apply -f consoleclidownload-virtctl-demo.yaml
